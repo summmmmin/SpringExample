70d5aa73360356f5ff3d9e4bec09f5e10360c0a3
mini338848@gmail.com <user@DESKTOP-9HPM1AG>
2023-06-13 11:22:19 +0900

    spring security

---
+++ ex230608/src/main/java/com/yedam/app/user/service/UserVO.java
@@ -0,0 +1,58 @@
+package com.yedam.app.user.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserVO implements UserDetails {
+
+	private String id;
+	private String password;
+	private String role;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> auth = new ArrayList<>();
+		auth.add(new SimpleGrantedAuthority(this.role));
+		return auth;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return id;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- ex230608/src/main/webapp/top.jsp
+++ ex230608/src/main/webapp/top.jsp
@@ -1,19 +1,30 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
+<sec:csrfMetaTags/>
 <title>Insert title here</title>
 </head>
 <body>
 	<h1>톱 페이지입니다.</h1>
 	<ul>
 		<li><a href="user/user.jsp">일반 사용자용 페이지로</a></li>
-		<li><a href="admin/admin.jsp">관리자 전용 페이지로</a></li>
+		<sec:authorize access="hasAuthority('ROLE_ADMIN')">
+			<li><a href="admin/admin.jsp">관리자 전용 페이지로</a></li>	
+		</sec:authorize>
 	</ul>
-	<form action="logout" method="post">
-		<button>로그아웃</button>
-	</form>
+	
+	<sec:authorize access="isAuthenticated()">
+		<form action="logout" method="post">
+			<sec:csrfInput/>
+			<button>로그아웃</button>
+		</form>	
+	</sec:authorize>
+	
+	<input type="hidden" name="csrf_name" value="${_csrf.parameterName }">
+	<input type="hidden" name="csrf_val" value="${_csrf.token }">
 </body>
 </html>
